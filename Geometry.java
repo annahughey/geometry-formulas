c83be78c346dffb23933274089ff2d236cce8b92
annahughey <37604536+annahughey@users.noreply.github.com>
2018-08-26 18:35:11 -0400

    Add files via upload

--- Geometry.java
+++ Geometry.java
@@ -45,8 +45,52 @@ public class Geometry
         
         System.out.println(ta1);
         System.out.println(ta2);
+           
+    }
+    
+    public static double trapezoidArea(double base1, double base2, double height)
+    {
+        double area = (base1 + base2) / 2.0 * height;
         
-        double ra1 = double rectangleArea(5.2, 9.3);
-        System.out.println(ra1);    
+        return area;
+    }
+    
+    public static double rectangularPrismVolume(double length, double width, double height)
+    {
+        double volume = length * width * height;
+        
+        return volume;
+    }
+    
+    public static double coneVolume(double radius)
+    {
+        return 4.0 / 3.0 * Math.PI * Math.pow(radius, 3);
+    }
+    
+    public static double rectangularPrismSurfaceArea(double width, double length, double height)
+    {
+        return (width * length + height * length + height * width) /2.0;
+    }
+    
+    public static double sphereSurfaceArea(double radius)
+    {
+        return 4.0 * Math.PI * Math.pow(radius, 2);
+    }
+    
+    public static double hypotenuseRightTriangle(double leg1, double leg2, double hypotenuse)
+    {
+        (Math.pow(leg1, 2) + Math.pow(leg2, 2)) = hypotenuse;
+        return Math.sqrt(double hypotenuse);
+    }
+    
+    public static double distanceBetween2Points(double x1, double y1, double x2, double y2, double dividend)
+    {
+        Math.pow((x2 - x1), 2) + Math.pow ((y2 - y1), 2) = dividend;
+        return Math.sqrt(double dividend);
+    }
+    
+    public static double slopeBetween2Points (double x1, double x2, double y1, double y2)
+    {
+        return (y2 - y1) / (x2 - x1);
     }
 }
